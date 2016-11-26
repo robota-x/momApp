2ce2cc83b500e6619f3998b0f5ee23ce33216456
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-11-26 20:31:55 +0000

    add date to data structure

--- www/js/services.js
+++ www/js/services.js
@@ -9,13 +9,15 @@ angular.module('starter.controllers')
             name: 'Spaghetti with meatballs',
             description: 'sample description',
             imageSrc: 'https://marvelapp-live.storage.googleapis.com/canvas/2016/11/c917926bcff94cdab7b87552741ac18b.jpg',
-            price: 9.45
+            price: 9.45,
+            date: 'Mon 28th Nov'
           },
         '1': {
             name: 'Risotto',
             description: 'sample description',
             imageSrc: 'https://marvelapp-live.storage.googleapis.com/canvas/2016/11/d544e56f3e3b48fca0240cca37a1f499.jpg',
-            price: 12.33
+            price: 12.33,
+            date: 'Tue 29th Nov'
           },
       }
     }
